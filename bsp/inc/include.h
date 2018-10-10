ebda5d647ee220c147a9d92346c7846192fafa1d
596142041 <wcj53636746@126.com>
2018-10-10 20:19:21 +0800

    系统头文件

---
+++ bsp/inc/include.h
@@ -0,0 +1,20 @@
+/********************************************************
+ *文件名:include.h
+ *创建时间:2018-10-09
+ *作者: 皇甫仁和
+ ********************************************************/
+#ifndef TMS320F2808_USER_INC_INCLUDE_H_
+#define TMS320F2808_USER_INC_INCLUDE_H_
+#include "DSP280x_Device.h"     // DSP280x Headerfile Include File
+#include "DSP280x_Examples.h"   // DSP280x Examples Include File
+#include "IQmathLib.h"
+#include "define.h"
+#include "delay.h"
+typedef struct
+{
+	u16 sys_time_cnt;//表示系统运行时间;
+	u8  sys_state;//表示系统运行状态
+	u16 ADC_sample_cnt;
+}Sys_Register;
+extern Sys_Register sys_reg;
+#endif /* TMS320F2808_USER_INC_INCLUDE_H_ */
