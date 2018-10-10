cd7a94c636f2a41e8483ceb1386223bb40ee279c
596142041 <wcj53636746@126.com>
2018-10-10 20:18:25 +0800

    继电器测试相关代码

---
+++ bsp/inc/relay.h
@@ -0,0 +1,15 @@
+/********************************************************
+ *文件名:relay.h
+ *创建时间:2018-10-09
+ *作者: 皇甫仁和
+ ********************************************************/
+#ifndef USER_INC_RELAY_H_
+#define USER_INC_RELAY_H_
+#include "include.h"
+void ReLay_GPIO_Config(void);
+void Relay_ON(void);
+void Relay_OFF(void);
+
+
+
+#endif /* USER_INC_RELAY_H_ */
