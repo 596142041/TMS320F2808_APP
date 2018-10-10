bbdf5fec47c5013c88daefeeb5d9ab2c19c2aff5
596142041 <wcj53636746@126.com>
2018-10-10 20:15:37 +0800

    CAN的应用层相关代码,

---
+++ CAN_APP/inc/CAN_APP.h
@@ -0,0 +1,110 @@
+/********************************************************
+ *文件名:CAN_APP.h
+ *创建时间:2018-10-09
+ *作者: 皇甫仁和
+ ********************************************************/
+#ifndef CAN_APP_INC_CAN_APP_H_
+#define CAN_APP_INC_CAN_APP_H_
+#include "include.h"
+#include "data.h"
+#include "stdint.h"
+#include "CANA.h"
+#include "Flash.h"
+#define APP_START_ADDR   ((uint32_t)0x3E8010)
+#define APP_INFO_ADDR    ((uint32_t)0x3E8000)
+#define Boot__START_ADDR ((uint32_t)0x3F7FF6)//0x3F7FF6
+#define CAN_BL_APP       0xAAAAAA
+#define CAN_BL_BOOT      0x555555
+#define DEVICE_ADDR      0x120//设备地址
+#define CMD_WIDTH        0x04
+#define ADDR_WIDTH       0x0C
+//--------------------------------------------------
+//---以下宏定义是对芯片型号进行宏定义
+#define TMS320F28335     0x01
+#define TMS320F2808      0x02
+#define STM32F407IGT6    0x03
+//---------------------------------------------------------------------------------
+//故障信息列表
+#define DEVICE_ADDR_ERROR  0xA0
+#define ERASE_ERROR        0xA1
+#define WRITE_ERROR        0xA2
+#define READ_LEN_ERROR     0xA3
+#define MSG_DATA_LEN_ERROR 0xA4
+//---------------------------------------------------
+typedef  void (*pFunction)(void);
+typedef struct _Device_INFO
+{
+	union
+	{
+		unsigned short int all;
+		struct
+		{
+			unsigned short int Device_addr:	12;
+			unsigned short int reserve:	4;
+		}bits;//设备地址
+	}Device_addr;
+	union
+	{
+		unsigned long int all;
+		struct
+		{
+			unsigned long int FW_type:24;
+			unsigned long int Chip_Value:8;
+		}bits;
+	}FW_TYPE;
+	union
+	{
+		unsigned long  int all;
+		struct
+		{
+			unsigned long  int Version:	7;//固件版本
+			unsigned long  int date:	5;//日期
+			unsigned long  int month:	4;//月
+			unsigned long  int year:	16;//年
+		}bits;
+	}FW_Version;//固件版本
+}Device_INFO;
+typedef struct _bootloader_data
+{
+	union
+	{
+		u32 all;
+		struct
+		{
+			u16 cmd :4; //命令
+			u16 addr :12; //设备地址
+			u16 reserve :16; //保留位
+		} bit;
+	} ExtId; //扩展帧ID
+	unsigned char IDE;   //帧类型,可为：CAN_ID_STD(标准帧),CAN_ID_EXT(扩展帧)
+	unsigned char DLC;  //数据长度，可为0到8;
+	u8 data[8];
+} bootloader_data;
+typedef struct _Boot_CMD_LIST
+{
+	//Bootloader相关命令
+	unsigned char Read;         //读取flash数据
+	unsigned char Erase;        //擦出APP储存扇区数据
+	unsigned char Write;        //以多字节形式写数据
+	unsigned char Check;        //检测节点是否在线，同时返回固件信息
+	unsigned char Excute;       //执行固件
+	unsigned char WriteInfo;    //设置多字节写数据相关参数（写起始地址，数据量）
+	unsigned char SetBaudRate;  //设置节点波特率
+	//节点返回状态,关键
+	unsigned char CmdFaild;     //命令执行失败
+	unsigned char CmdSuccess;   //命令执行成功
+} Boot_CMD_LIST;
+extern Boot_CMD_LIST cmd_list;
+extern bootloader_data Bootloader_data;
+extern Device_INFO DEVICE_INFO;
+extern Uint16 app_check[2];
+void __disable_irq(void);
+void __enable_irq(void);
+void __set_PRIMASK(u8 state);
+
+unsigned short int CRCcalc16 (unsigned char *data,unsigned short int len);
+void CAN_BOOT_JumpToApplication(uint32_t Addr);
+void CAN_BOOT_ExecutiveCommand(CanRxMsg *pRxMessage);
+
+
+#endif /* CAN_APP_INC_CAN_APP_H_ */
---
+++ CAN_APP/src/CAN_APP.c
@@ -0,0 +1,245 @@
+/********************************************************
+ *文件名:CAN_APP.c
+ *创建时间:2018-10-09
+ *作者: 皇甫仁和
+ ********************************************************/
+#include "CAN_APP.h"
+#define DATA_LEN  520
+#define READ_MAX  256
+bootloader_data Boot_ID_info;
+u8  can_cmd         = (u8 )0x00;//ID的bit0~bit3位为命令码
+u16 can_addr        = (u16)0x00;;//ID的bit4~bit15位为节点地址
+u32 exe_type        = (u32)0x00;
+u32 read_addr       = (u32)0x00;//读取数据起始地址
+u32 read_len        = (u32)0x00;//读取数据长度
+u32 data_index      = (u32)0x00;//数据指针
+Uint16 app_check[2] = {0xFFFF,0xF2F3};
+u8	   data_temp[DATA_LEN*2];
+Uint16 read_temp[READ_MAX];
+Boot_CMD_LIST cmd_list =
+{
+	.Read        = 0x0A, //读取flash数据
+	.Erase       = 0x00, //擦除APP区域数据
+	.Write       = 0x02,//以多字节形式写数据
+	.Check       = 0x03,//检测节点是否在线，同时返回固件信息
+	.Excute      = 0x05,//执行固件
+	.CmdFaild    = 0x09,//命令执行失败
+	.WriteInfo   = 0x01,//设置多字节写数据相关参数(写起始地址,数据量)
+	.CmdSuccess  = 0x08,//命令执行成功
+	.SetBaudRate = 0x04,//设置节点波特率
+};
+
+Device_INFO DEVICE_INFO =
+{
+ .FW_Version.bits.Version       = 21,
+ .FW_Version.bits.date          = 9,
+ .FW_Version.bits.month         = 10,
+ .FW_Version.bits.year          = 2018,
+ .Device_addr.bits.reserve      = 0x00,
+ .Device_addr.bits.Device_addr  = DEVICE_ADDR,
+ .FW_TYPE.bits.FW_type          = CAN_BL_APP,
+ .FW_TYPE.bits.Chip_Value       = TMS320F2808
+};
+void __disable_irq(void)
+{
+	DINT;
+	DRTM;
+}
+void __enable_irq(void)
+{
+	EINT;
+	ERTM;
+}
+void __set_PRIMASK(u8 state)
+{
+	if(state == 1)
+	{
+		__disable_irq();
+	}
+	else if(state == 0)
+	{
+		__enable_irq();
+	}
+	else
+	{
+		return;
+	}
+}
+unsigned short int CRCcalc16 (unsigned char *data,unsigned short int len)
+{
+	int i;
+	unsigned short int crc_res =  0xFFFF;
+	while(len--)
+	{
+		crc_res^=*data++;
+		for(i = 0;i < 8;i++)
+		{
+			if(crc_res&0x01)
+			{
+				crc_res = (crc_res>>1)^0xa001;
+			}
+			else
+			{
+				crc_res = (crc_res>>1);
+			}
+		}
+	}
+	return crc_res;
+}
+void CAN_BOOT_JumpToApplication(uint32_t Addr)
+{
+	(*((void(*)(void))(Addr)))();
+}
+#pragma CODE_SECTION(CAN_BOOT_ExecutiveCommand,"ramfuncs");
+void CAN_BOOT_ExecutiveCommand(CanRxMsg *pRxMessage)
+{
+	u8 i;
+	CanTxMsg TxMessage;//发送对应消息
+	for(i = 0;i <8;i++)
+	{
+		TxMessage.CAN_Tx_msg_data.msg_byte.data[i] = 0x00;
+	}
+	TxMessage.DLC              = 0x01;
+	TxMessage.CAN_num          = CANA;
+	TxMessage.MBox_num         = 0x03;
+	TxMessage.Tx_timeout_cnt   = 0x00;
+	TxMessage.SAE_J1939_Flag   = 0x00;
+	TxMessage.ExtId.bit.resved = 0x00;
+	TxMessage.IDE              = CAN_ID_EXT;
+	//获取地址信息
+	Boot_ID_info.ExtId.all     = pRxMessage->ExtId.bit.ExtId;
+	can_cmd                    = Boot_ID_info.ExtId.bit.cmd;
+	can_addr                   = Boot_ID_info.ExtId.bit.addr;
+	if((can_addr!=DEVICE_ADDR))
+	{
+		TxMessage.DLC                              = 0x01;
+		TxMessage.CAN_Tx_msg_data.msg_byte.data[0] = 0x01;
+		TxMessage.ExtId.bit.ExtId                  = (DEVICE_INFO.Device_addr.bits.Device_addr<<CMD_WIDTH)|cmd_list.CmdFaild;
+		CAN_Tx_Msg(&TxMessage);
+		return;
+	}
+	//CMD_List.Check，节点在线检测
+	//节点收到该命令后返回固件版本信息和固件类型，
+	//该命令在Bootloader程序和APP程序都必须实现
+	if(can_cmd == cmd_list.Check)//DSP中尚未实现,相对比较容易实现,主要是为实现APP再次更新应用程序
+	{
+		if(can_addr != 0x00)
+		{
+			TxMessage.DLC                              = 8;
+			TxMessage.ExtId.bit.ExtId                  = (DEVICE_INFO.Device_addr.bits.Device_addr<<CMD_WIDTH)|cmd_list.CmdSuccess;
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[0] = (u8)((DEVICE_INFO.FW_Version.all>>0x18)&0xFF);
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[1] = (u8)((DEVICE_INFO.FW_Version.all>>0x10)&0xFF);
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[2] = (u8)((DEVICE_INFO.FW_Version.all>>0x08)&0xFF);
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[3] = (u8)((DEVICE_INFO.FW_Version.all>>0x00)&0xFF);
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[4] = (u8)((DEVICE_INFO.FW_TYPE.bits.FW_type>>0x10)&0xFF);
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[5] = (u8)((DEVICE_INFO.FW_TYPE.bits.FW_type>>0x08)&0xFF);
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[6] = (u8)((DEVICE_INFO.FW_TYPE.bits.FW_type>>0x00)&0xFF);
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[7] = (u8)((DEVICE_INFO.FW_TYPE.bits.Chip_Value>>0)&0xFF);
+			CAN_Tx_Msg(&TxMessage);
+		}
+	}
+	//cmd_list.read,读取flash数据,
+	//该命令是用于读取内部存储器数据
+	//该命令在Bootloader和APP程序中国必须实现
+	if(can_cmd == cmd_list.Read)
+	{
+		if(pRxMessage->DLC != 8)
+		{
+			TxMessage.DLC                              = 0x02;
+			TxMessage.ExtId.bit.ExtId                  = (DEVICE_INFO.Device_addr.bits.Device_addr<<CMD_WIDTH)|cmd_list.CmdFaild;
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[0] = (u8)cmd_list.Read;//主版本号，两字节
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[1] = (u8)MSG_DATA_LEN_ERROR;
+			CAN_Tx_Msg(&TxMessage);
+			return;
+		}
+		read_addr =  (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[0])&0xFFFFFFFF)<<0x18)|\
+					 (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[1])&0x00FFFFFF)<<0x10)|\
+					 (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[2])&0x0000FFFF)<<0x08)|\
+					 (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[3])&0x000000FF)<<0x00);
+		read_len  =  (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[4])&0xFFFFFFFF)<<0x18)|\
+					 (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[5])&0x00FFFFFF)<<0x10)|\
+				     (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[6])&0x0000FFFF)<<0x08)|\
+				     (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[7])&0x000000FF)<<0x00);
+		if(read_len > READ_MAX)
+		{
+			TxMessage.DLC                              = 0x02;
+			TxMessage.ExtId.bit.ExtId                  = (DEVICE_INFO.Device_addr.bits.Device_addr<<CMD_WIDTH)|cmd_list.CmdFaild;
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[0] = (u8)cmd_list.Read;//主版本号，两字节
+			TxMessage.CAN_Tx_msg_data.msg_byte.data[1] = (u8)READ_LEN_ERROR;
+			CAN_Tx_Msg(&TxMessage);
+			return;
+		}
+		u16 read_len_temp  = 0;
+		if(read_len%2 == 0)//因为每次只能读取N个字
+		{
+			read_len_temp = read_len;
+		}
+		else
+		{
+			read_len_temp = read_len+1;
+		}
+		read_len_temp = read_len_temp>>1;
+		__set_PRIMASK(1);
+		Flash_RD(read_addr,read_temp,read_len_temp);
+		__set_PRIMASK(0);
+		for(i = 0;i <read_len_temp;i++)
+		{
+			data_temp[i*2+0] = (read_temp[i]>>8)&0xFF;
+			data_temp[i*2+1] = read_temp[i]&0xFF;
+		}
+		data_index = 0;
+		i = 0;
+		while(data_index < read_len)
+		{
+			int temp;
+			temp = read_len - data_index;
+			if (temp >= 8)
+			{
+				TxMessage.DLC             = 0x08;
+				TxMessage.ExtId.bit.ExtId = (DEVICE_INFO.Device_addr.bits.Device_addr<<CMD_WIDTH)|cmd_list.CmdSuccess;
+				for (i = 0; i < TxMessage.DLC; i++)
+				{
+					TxMessage.CAN_Tx_msg_data.msg_byte.data[i] = data_temp[data_index];
+					data_index++;
+				}
+
+			}
+			else
+			{
+				TxMessage.DLC             = temp;
+				TxMessage.ExtId.bit.ExtId = (DEVICE_INFO.Device_addr.bits.Device_addr<<CMD_WIDTH)|cmd_list.CmdSuccess;
+				for (i = 0; i < TxMessage.DLC; i++)
+				{
+					TxMessage.CAN_Tx_msg_data.msg_byte.data[i] = data_temp[data_index];
+					data_index++;
+				}
+			}
+			CAN_Tx_Msg(&TxMessage);
+		}
+		data_index = 0;
+		return;
+	}
+	//CMD_List.Excute，控制程序跳转到指定地址执行
+	//该命令在Bootloader和APP程序中都必须实现
+	if(can_cmd == cmd_list.Excute)//该命令在DSP中已经实现
+	{
+		__set_PRIMASK(1);
+		Flash_WR(APP_INFO_ADDR,(Uint16 *)0xF3F5,2);
+		__set_PRIMASK(0);
+		exe_type = (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[0])&0xFFFFFFFF)<<0x10)|\
+				   (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[1])&0x00FFFFFF)<<0x08)|\
+				   (((u32)(pRxMessage->CAN_Rx_msg_data.msg_byte.data[2])&0x0000FFFF)<<0x00);
+		if(exe_type == CAN_BL_BOOT)
+		{
+			if((*((uint32_t*)Boot__START_ADDR)!=0xFFFFFFFF))
+			{
+				CAN_BOOT_JumpToApplication(Boot__START_ADDR);
+			}
+		}
+		return;
+	}
+	return;
+
+}
+
+
